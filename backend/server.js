e192d5c68f267007d767c6630786e14235ec4e57
pankaj kumar <pk11345rai@gmail.com>
2025-08-22 12:31:20 +0530

    cors fix

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,15 @@
 const app = require('./app');
+const cors = require("cors")
+
+// app.use(cors())
+app.use(
+  cors({
+    origin: "https://ghumophiro-1.onrender.com",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true, // only if you use cookies/auth headers
+  })
+);
 
 app.listen(8000, () => {
     console.log('Server started on port 8000'); 
